808428562f64a212b3d97be5dc85b05a6ee93c4f
m.r.esmaeili <info@mreweb.ir>
2023-09-12 11:24:21 +0330

    update helper

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,5 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
-
-
 use MiladRahimi\Jwt\Generator;
 use MiladRahimi\Jwt\Parser;
 use MiladRahimi\Jwt\Cryptography\Algorithms\Hmac\HS256;
@@ -9,16 +7,11 @@ use MiladRahimi\Jwt\Exceptions\ValidationException;
 use MiladRahimi\Jwt\Validator\DefaultValidator;
 use MiladRahimi\Jwt\Validator\Rules\EqualsTo;
 use MiladRahimi\Jwt\Validator\Rules\NewerThan;
-
-
-
 class Welcome extends CI_Controller{
-
 	public function __construct(){
 		parent::__construct();
 		$this->load->helper('auth');
 	}
-
 	public function index(){
 		$this->load->view('welcome_message');
 	}
@@ -26,23 +19,23 @@ class Welcome extends CI_Controller{
 		require 'vendor/autoload.php';
 		$signer = new HS256($this->config->item('HS256KEY'));
 		$generator = new Generator($signer);
-		$jwt = $generator->generate(['id' => 666, 'is-admin' => true , 'expire_time' =>time()+200]);
+		$jwt = $generator->generate([
+            'id' => 666,
+            'is-admin' => true ,
+            'roles' => ['ADMIN' , 'AGENCY'] ,
+            'expire_time' =>time()+200
+        ]);
 		echo json_encode([
 			'success' => true,
 			'message' => 'Token Created',
 			'token' => $jwt
 		]);
 	}
-
 	public function is_ok(){
 		checkToken();
 	}
-
 	public function get_info(){
 		$info = getTokenInfo();
 		var_dump($info);
 	}
-
-
-
 }
