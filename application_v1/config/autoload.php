8c8d0ce7114332384df50bc0552233a49d754c5d
mohamadreza esmaeeli <esmaeeli1923@gmail.com>
2023-10-30 12:12:46 +0330

    update git ignore

--- application_v1/config/autoload.php
+++ application_v1/config/autoload.php
@@ -1,10 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $autoload['packages'] = array();
-/* دریافت کتابخانه های مورد نیاز در سراسر پروژه */
 $autoload['libraries'] = array('session','database','form_validation','JDateTime','encryption' , 'Uuid');
 $autoload['drivers'] = array();
-/* دریافت توابع مورد نیاز در سراسر پروژه */
 $autoload['helper'] = array('url','string','security','html','form','utility/security','utility/pipe','utility/db','cookie');
 $autoload['config'] = array('');
 $autoload['language'] = array();
--- application_v1/helpers/cors.php
+++ application_v1/helpers/cors.php
@@ -5,12 +5,9 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
 	header('Access-Control-Max-Age: 30');
 }
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
 	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
 		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
 	if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
+		header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");	
 	exit(0);
 }
